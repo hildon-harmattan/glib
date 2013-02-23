526a0d0924e47c67cd1c7efda8a7ac1d10e3683e
Michael Demetriou <qwazix@gmail.com>
2013-02-23 20:05:01 +0200

    changes to build for harmattan

--- autogen.sh
+++ autogen.sh
@@ -7,12 +7,6 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd "$srcdir"
 
-GTKDOCIZE=`which gtkdocize`
-if test -z $GTKDOCIZE; then
-        echo "*** No GTK-Doc found, please install it ***"
-        exit 1
-fi
-
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
         echo "*** No autoreconf found, please install it ***"
@@ -24,7 +18,6 @@ fi
 # regenerated from their corresponding *.in files by ./configure anyway.
 touch README INSTALL
 
-gtkdocize || exit $?
 autoreconf --force --install --verbose || exit $?
 
 cd "$olddir"
